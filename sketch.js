ce7570bedd16526580748e4f2e2120e798f0971b
Prakhar272006 <77096052+Prakhar272006@users.noreply.github.com>
2021-01-20 15:12:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,30 +7,45 @@ var jerrySearching,jerryTeazing,jerryGift
 
 function preload() {
     //load the images here
-    gardenImg = loadImage("garden.png")
-    tomSitting = loadImage("tomOne.png")
-    tomRunning = loadAnimation("tomTwo.png","tomThree.png")
-    tomStanding = loadImage("tomFour.png")
-    jerryGift = loadImage("jerryOne.png")
-    jerryTeazing = loadAnimation("jerryTwo","jerryThree")
-    jerrySearching = loadImage("jerryFour.png")
+    gardenImg = loadImage("images/garden.png")
+    tomSitting = loadAnimation("images/tomOne.png")
+    tomRunning = loadAnimation("images/tomTwo.png","images/tomThree.png")
+    tomStanding = loadAnimation("images/tomFour.png")
+    jerryGift = loadAnimation("images/jerryOne.png")
+    jerryTeazing = loadAnimation("images/jerryTwo","images/jerryThree")
+    jerrySearching = loadAnimation("images/jerryFour.png")
 }
 
 function setup(){
     createCanvas(1000,800);
-    garden = createSprite(500,400)
-    garden.addImage (gardenImg)
+
+    garden = createSprite(500,400,250,200)
+    garden.addImage(gardenImg)
     //create tom and jerry sprites here
-    tom = createSprite (850,700,20,20)
-    tom.addImage(tomStanding)
-    jerry = createSprite(250,700,20,20)
-    jerry.addImage(jerryGift)
+    tom = createSprite (850,600,20,20)
+    tom.addAnimation(tomSitting);
+    tom.scale = 0.2
+    jerry = createSprite(100,600,20,20)
+    jerry.addAnimation(jerryGift);
+    jerry.scale = 0.2
+    //
+
+ 
 }
 
 function draw() {
 
     background(255);
     //Write condition here to evalute if tom and jerry collide
+    if(tom.x-jerry.x <(tom.width - jerry.width)/2){
+      tom.velocityX = 0
+
+       tom.addAnimation("sitting",tomSitting)
+        
+        tom.changeAnimation(tomSitting)
+        jerry.addAnimation(jerryTeazing)
+        jerry.changeAnimation(jerryTeazing)
+    }
 
     drawSprites();
 }
@@ -39,6 +54,12 @@ function draw() {
 function keyPressed(){
 
   //For moving and changing animation write code here
+  if(keyCode == LEFT_ARROW){
+      tom.velocityX = -5
+      tom.addAnimation("catRunning",tomRunning)
+      tom.changeAnimation(tomRunning)
+  
+  }
 
 
 }
